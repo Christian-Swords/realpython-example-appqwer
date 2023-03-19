07f8a291a826b146b5d8919199aa2bfb928fa6e1
Christian-Swords <c.swords.9102@tamu.edu>
2023-03-19 15:53:15 -0500

    12tenth commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] ='uploads'
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,3 +15,17 @@ def show_number():
 def display_number(number):
     return render_template('display_number.html', number=number)
 
+@app.route('/uploadUsersFile', methods = ['POST'])
+def uploadUsersFile():
+    file = request.files['video']
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('show_video', filename=filename))
+
+@app.route('/video/<filename>')
+def show_video(filename):
+    return render_template('video.html',filename=filename)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
