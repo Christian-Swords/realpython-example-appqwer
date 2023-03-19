a60f620f850134b8048cf1fce73b156cb8978c5c
Christian-Swords <c.swords.9102@tamu.edu>
2023-03-19 15:15:30 -0500

    eigth commit

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hellocddsd Wssld! this is the newest version"
+    return render_template('index.html')
+
+@app.route('/show_number', methods=['POST'])
+def show_number():
+    number = request.form['number']
+    return redirect(url_for('display_number', number=number))
+
+@app.route('/display_number/<number>')
+def display_number(number):
+    return render_template('display_number.html', number=number)
