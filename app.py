ca639738e1d57d2665e17f32029e28ccd16597ac
Christian-Swords <c.swords.9102@tamu.edu>
2023-03-20 14:10:20 -0500

    34th

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def uploadUsersFile():
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv2.CAP_PROP_FPS))
-    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    fourcc = cv2.VideoWriter_fourcc(*'avc1')
     outputFileName = 'annotatedVideo.mp4'
     out = cv2.VideoWriter('uploads/' + outputFileName, fourcc, fps, (width, height))
     while True:
