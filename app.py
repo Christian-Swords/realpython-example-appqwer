b2c90644ef1e907ff7a5c99c0c503f5516c9c07c
Christian-Swords <c.swords.9102@tamu.edu>
2023-03-19 20:18:58 -0500

    27th

--- app.py
+++ app.py
@@ -1,46 +1,7 @@
-# from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash, get_flashed_messages
-# import os
-
-# app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] ='uploads'
-# app.secret_key = 'secret_key'
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/show_number', methods=['POST'])
-# def show_number():
-#     number = request.form['number']
-#     return redirect(url_for('display_number', number=number))
-
-# @app.route('/display_number/<number>')
-# def display_number(number):
-#     return render_template('display_number.html', number=number)
-
-# @app.route('/uploadUsersFile', methods = ['POST'])
-# def uploadUsersFile():
-#     file = request.files['video']
-#     filename = file.filename
-#     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#     feedback = "Too Low"
-#     flash(feedback)
-#     return redirect(url_for('show_video', filename=filename))
-
-# @app.route('/video/<filename>')
-# def show_video(filename):
-#     feedback = None
-#     feedback_messages = get_flashed_messages()
-#     if feedback_messages:
-#         feedback = feedback_messages[0]
-#     return render_template('video.html',filename=filename, feedback=feedback)
-
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash, get_flashed_messages
 import os
 import cv2
+import mediapipe as mp
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] ='uploads'
