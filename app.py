4e294b527ab6a0a62c60bf15797078da43f72f32
Christian-Swords <c.swords.9102@tamu.edu>
2023-03-23 16:46:59 -0500

    54?th

--- app.py
+++ app.py
@@ -57,6 +57,54 @@ POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElb
                ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
                ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
 
+thr = 0.2
+
+def getPose(frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+    inWidth = frameWidth 
+    inHeight = frameHeight
+    
+    net.setInput(cv2.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+    out = net.forward()
+    out = out[:, :19, :, :]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
+
+    assert(len(BODY_PARTS) == out.shape[1])
+
+    points = []
+    for i in range(len(BODY_PARTS)):
+        # Slice heatmap of corresponging body's part.
+        heatMap = out[0, i, :, :]
+
+        # Originally, we try to find all the local maximums. To simplify a sample
+        # we just find a global one. However only a single pose at the same time
+        # could be detected this way.
+        _, conf, _, point = cv2.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / out.shape[3]
+        y = (frameHeight * point[1]) / out.shape[2]
+        # Add a point if it's confidence is higher than threshold.
+        points.append((int(x), int(y)) if conf > thr else None)
+
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert(partFrom in BODY_PARTS)
+        assert(partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+
+        if points[idFrom] and points[idTo]:
+            cv2.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+            cv2.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+            cv2.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+
+    t, _ = net.getPerfProfile()
+    freq = cv2.getTickFrequency() / 1000
+    return frame
+
+
+
 app.config['UPLOAD_FOLDER'] ='uploads'
 app.secret_key = 'secret_key'
 @app.route('/')
@@ -92,7 +140,7 @@ def uploadUsersFile():
     #detector = PoseDetector()
     while True:
         ret,frame = cap.read()
-        
+        print("next frame")
         if not ret:
             break
         #flipped_frame = cv2.flip(frame,1)
@@ -100,11 +148,14 @@ def uploadUsersFile():
         #frame, p_landmarks, p_connections = detector.findPose(frame, False)
         cv2.line(frame,(100,100),(200,200),(0,0,255), thickness=3,lineType=cv2.LINE_AA)
         #mp.drawing_utils.draw_landmarks(frame, p_landmarks, p_connections)
+        #if (feedback % 6 == 0):#only detect a pose every 6 frames, to speed up computational times
+        #    frame = getPose(frame)
         out.write(frame)
         
 
 
         feedback += 1
+        print("frame number: ",feedback)
     cap.release()
     out.release()
     os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
